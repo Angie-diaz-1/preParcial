d25837c13e686bfd6bcc5f84c5456138ec1c3871
Angie-diaz-1 <diaz.angie.2352@eam.edu.co>
2024-02-29 18:24:48 -0500

    creacion proyecto

---
+++ src/main/java/com/eam/PreparcialPrimerCorteApplication.java
@@ -0,0 +1,13 @@
+package com.eam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreparcialPrimerCorteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreparcialPrimerCorteApplication.class, args);
+	}
+
+}
